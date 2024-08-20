ad21f723494c99b76f551470d1a8dcb21d97f3eb
inal <balkarov05@gmail.com>
2024-08-20 22:29:25 +0300

    use run settings

---
+++ src/core/config.py
@@ -0,0 +1,21 @@
+
+from pydantic_settings import BaseSettings
+from pydantic import BaseModel
+
+
+class RunConfig(BaseModel):
+    host: str = "0.0.0.0"
+    port: int = 8000
+    reload: bool = True
+
+
+class ApiPrefix(BaseModel):
+    prefix: str = "/api"
+
+
+class Settings(BaseSettings):
+    run: RunConfig = RunConfig()
+    api: ApiPrefix = ApiPrefix()
+
+
+settings = Settings()
