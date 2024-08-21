aa37c21ec4fa7ab63846022fc5f7baf0864cfb83
inal <balkarov05@gmail.com>
2024-08-21 11:16:45 +0300

    created database helper and database config

--- src/core/config.py
+++ src/core/config.py
@@ -1,6 +1,7 @@
 
+from django import db
 from pydantic_settings import BaseSettings
-from pydantic import BaseModel
+from pydantic import BaseModel, PostgresDsn
 
 
 class RunConfig(BaseModel):
@@ -13,9 +14,18 @@ class ApiPrefix(BaseModel):
     prefix: str = "/api"
 
 
+class DataBaseConfig(BaseModel):
+    url: PostgresDsn
+    url: str,
+    echo: bool = False,
+    echo_pool: bool = False,
+    pool_size: int = 50,
+    max_overflow: int = 10,
+
+
 class Settings(BaseSettings):
     run: RunConfig = RunConfig()
     api: ApiPrefix = ApiPrefix()
-
+    db: DataBaseConfig
 
 settings = Settings()
