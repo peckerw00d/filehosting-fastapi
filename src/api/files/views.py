afd42a071d61d967bd0edc7d3a298c1032abf9ee
inal <balkarov05@gmail.com>
2024-08-27 01:34:38 +0300

    added upload file view

--- src/api/files/views.py
+++ src/api/files/views.py
@@ -1,3 +1,5 @@
+from distutils.command import upload
+import shutil
 from fastapi import APIRouter, Depends, File, UploadFile
 from sqlalchemy.ext.asyncio import AsyncSession
 
@@ -8,10 +10,25 @@ from . import services
 router = APIRouter(tags=["Files"])
 
 
-@router.post("/")
-async def get_file(
-    file: bytes = File(...),
-):
+@router.post("/get_file")
+async def get_file(file: bytes = File(...)):
     content = file.decode("utf-8")
     lines = content.split("\n")
     return {"data": lines}
+
+
+@router.post("/upload_file")
+async def upload_file(upload_file: UploadFile = File(...)):
+    upload_file.filename = upload_file.filename.lower()
+    
+    path = f"media/{upload_file.filename}"
+
+    with open(path, "wb+") as buffer:
+        shutil.copyfileobj(upload_file, buffer)
+    
+    return {
+        "file": upload_file,
+        "file": upload_file.filename,
+        "path": path,
+        "type": upload_file.content_type
+    }
