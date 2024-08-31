c032f89d95c616d5d7580f447b361aebe30deb84
inal <balkarov05@gmail.com>
2024-08-31 18:25:56 +0300

    added get file view

--- src/api/files/views.py
+++ src/api/files/views.py
@@ -3,19 +3,24 @@ from typing import List
 from fastapi import APIRouter, Depends, File, UploadFile
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from api.files import dependencies
 from core.models.db_helper import db_helper
-from .services import (
-    read_file_service, 
-    upload_file_service, 
-    upload_multiple_files_service,
-)
+from core.models.files import FileModel
+from . import services
 
 router = APIRouter(tags=["Files"])
 
 
 @router.post("/read-file")
 async def read_file(file: bytes = File(...)):
-    return await read_file_service(file=file)
+    return await services.read_file(file=file)
+
+
+@router.get("/get-file")
+async def get_file(
+    file: FileModel = Depends(dependencies.file_by_id)
+):
+    return await file
 
 
 @router.post("/upload-file")
@@ -23,7 +28,7 @@ async def upload_file(
     file: UploadFile = File(...), 
     session: AsyncSession = Depends(db_helper.session_getter),
 ):
-    return await upload_file_service(
+    return await services.upload_file(
         file=file,
         session=session,
 )
@@ -34,7 +39,7 @@ async def upload_multiple_files(
     files: List[UploadFile] = File(...), 
     session: AsyncSession = Depends(db_helper.session_getter),
 ):
-    return await upload_multiple_files_service(
+    return await services.upload_multiple_files(
         files=files,
         session=session,
     )
