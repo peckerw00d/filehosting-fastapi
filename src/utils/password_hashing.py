84ae7c239e815495f960cf994d6b46061df44e2e
inal <balkarov05@gmail.com>
2024-10-29 19:52:39 +0300

    created password hasing utils

---
+++ src/utils/password_hashing.py
@@ -0,0 +1,13 @@
+from typing_extensions import deprecated
+from passlib.context import CryptContext
+
+
+password_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+
+def get_hashed_password(password: str) -> str:
+    return password_context.hash(password)
+
+
+def verify_password(password: str, hash_password: str) -> bool:
+    return password_context.verify(password, hash_password)
