f3dcc20ede12c911fac8b35d5d5a595839a2ac7c
inal <balkarov05@gmail.com>
2025-01-12 00:34:03 +0300

    added conftest file

---
+++ tests/conftest.py
@@ -0,0 +1,34 @@
+import pytest_asyncio
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
+
+from src.config import settings
+from src.adapters.orm.models import Base
+from src.adapters.repository import SqlAlchemyRepository
+
+
+@pytest_asyncio.fixture()
+async def engine():
+    engine = create_async_engine(url=settings.test_db.url, echo=True)
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+    yield engine
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
+    await engine.dispose()
+
+
+@pytest_asyncio.fixture
+async def session(engine):
+    async_session = async_sessionmaker(bind=engine, expire_on_commit=False)
+    async with async_session() as session:
+        async with session.begin():
+            try:
+                yield session
+            finally:
+                await session.close()
+
+
+@pytest_asyncio.fixture
+def repository(session):
+    repository = SqlAlchemyRepository(session)
+    return repository
