c26c364d5cab36bec52fca5e4f6d61b08f1fb258
inal <balkarov05@gmail.com>
2025-01-21 12:59:55 +0300

    edited the file model

--- src/adapters/orm/models/files.py
+++ src/adapters/orm/models/files.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from uuid import uuid4
 
 from sqlalchemy import DateTime, Integer, String
 from .base import Base
@@ -9,8 +10,8 @@ from sqlalchemy.orm import Mapped, mapped_column
 class FileModel(Base):
     __tablename__ = "files"
 
-    filename: Mapped[str] = mapped_column(String)
-    filesize: Mapped[int] = mapped_column(Integer)
-    last_modified: Mapped[datetime] = mapped_column(DateTime(timezone=True))
-    etag: Mapped[str] = mapped_column(String)
-    content_type: Mapped[str] = mapped_column(String)
+    file_url: Mapped[str] = mapped_column(
+        String,
+        nullable=False,
+        default=lambda: str(uuid4()),
+    )
