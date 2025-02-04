6bebc76ed6ddb3135b09f069bc698a7ebccf8972
inal <balkarov05@gmail.com>
2025-02-04 12:54:08 +0300

    created new repositories

--- src/adapters/repository.py
+++ src/adapters/repository.py
@@ -2,6 +2,8 @@ from abc import ABC, abstractmethod
 
 import uuid
 
+from src.adapters.orm.models import FileModel, User, Session
+
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select, Result
 from sqlalchemy.orm import selectinload
@@ -17,31 +19,67 @@ class AbstractRepository(ABC):
         raise NotImplementedError
 
 
-class SqlAlchemyRepository(AbstractRepository):
+class FileRepository(AbstractRepository):
     def __init__(self, session: AsyncSession):
         self.session = session
 
-    async def add(self, entity):
-        self.session.add(entity)
-        return entity
+    async def add(self, file: FileModel):
+        self.session.add(file)
+        return file
 
-    async def get(self, entity, entity_id):
-        return await self.session.get(entity=entity, ident=entity_id)
+    async def get(self, file: FileModel, file_id: int):
+        return await self.session.get(entity=file, ident=file_id)
 
-    async def list(self, entity):
-        stmt = select(entity).order_by(entity.id)
-        result: Result = await self.session.execute(stmt)
-        entities = Result.scalars(result).all()
-        return list(entities)
-
-    async def delete(self, entity):
-        await self.session.delete(entity)
-
-    async def get_session_by_id(self, entity, session_id):
-        stmt = stmt = (
-            select(entity)
-            .where(entity.session_id == uuid.UUID(session_id))
-            .options(selectinload(entity.user))
-        )
+    async def get_by_url(self, file_url: str):
+        stmt = select(FileModel).where(FileModel.file_url == file_url)
         result = await self.session.execute(stmt)
         return result.scalars().first()
+
+    async def list(self):
+        stmt = select(FileModel).order_by(FileModel.id)
+        result = await self.session.execute(stmt)
+        return list(result.scalars().all())
+
+    async def delete(self, file: FileModel):
+        await self.session.delete(file)
+
+
+class UserRepository(AbstractRepository):
+    def __init__(self, session: AsyncSession):
+        self.session = session
+
+    async def add(self, user: User):
+        self.session.add(user)
+        return user
+
+    async def get(self, user: User, user_id: uuid.UUID):
+        stmt = select(User).where(User.id == user_id)
+        result = await self.session.execute(stmt)
+        return result.scalars().first()
+
+    async def list(self):
+        stmt = select(User).order_by(User.id)
+        result = await self.session.execute(stmt)
+        return list(result.scalars().all())
+
+    async def delete(self, user: User):
+        await self.session.delete(user)
+
+
+class SessionRepository(AbstractRepository):
+    def __init__(self, session: AsyncSession):
+        self.session = session
+
+    async def add(self, session: Session):
+        self.session.add(session)
+        return session
+
+    async def get(self, session: Session, session_id: uuid.UUID):
+        stmt = select(Session).where(Session.session_id == session_id)
+        result = await self.session.execute(stmt)
+        return result.scalars().first()
+
+    async def list(self):
+        stmt = select(Session).order_by(Session.session_id)
+        result = await self.session.execute(stmt)
+        return list(result.scalars().all())
